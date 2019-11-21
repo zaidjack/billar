78d64f5aecef83f1ec539616f24ea04b4ad581c7
zaidjack <51840226+zaidjack@users.noreply.github.com>
2019-11-21 15:07:36 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var http = require('http');
 var url = require('url');
 
+var nombres =[];
+
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true).query;
+  nombres.push(q.nombre);
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hola ' + q.nombre);
+  res.end('Lista de nombres ' + nombres.join());
 }).listen(process.env.PORT);
